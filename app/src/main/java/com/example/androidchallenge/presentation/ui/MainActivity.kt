d599f75254143b68156ca04d327025593587365e
oppo404155 <ahmedazap700>
2022-05-28 03:42:37 +0300

    initial build

---
+++ app/src/main/java/com/example/androidchallenge/presentation/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidchallenge.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import com.example.androidchallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
